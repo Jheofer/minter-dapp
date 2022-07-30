3aef83ae7b943bba423983767e544840eb10ca16
Johnathon Hoefer <johnathonwillhelp@gmail.com>
2022-07-30 02:18:25 -0600

    test

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -31,7 +31,7 @@ window.addEventListener("DOMContentLoaded", async () => {
     pagination: false,
     autoplay: true,
     direction: 'ttb',
-    height: "calc(100vh - 900px",
+    height: "calc(100vh - 90px)",
     width: '30vw',
     autoHeight: true,
   });
